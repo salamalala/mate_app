50a4e9f6dc74a845e2a5fa94abec47486cdbef47
salamalala <hello@ninaregli.ch>
2015-02-07 14:52:26 +0000

    correcting journeys form for typos

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :bookings
 
   devise_for :users
-  root to: "home#index"
+  root to: "journeys#index"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
