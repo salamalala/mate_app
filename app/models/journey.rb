f1ffb42a883bc20c690ba492e8257ff5956193d8
salamalala <hello@ninaregli.ch>
2015-02-09 17:03:55 +0000

    get end_port_longitude and latitude working in console

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -4,9 +4,9 @@ class Journey < ActiveRecord::Base
   has_many :users, through: :bookings
 
 
-  geocoded_by :start_full_address, latitude: :start_port_latitude, longitude: :start_longitude
+  geocoded_by :start_full_address, latitude: :start_port_latitude, longitude: :start_port_longitude
 
-  geocoded_by :end_full_address, latitude: :end_port_latitude, longitude: :end_longitude
+  geocoded_by :end_full_address, latitude: :end_port_latitude, longitude: :end_port_longitude
 
   #specifies when to do the actual geo coding. 
   after_validation :geocode
