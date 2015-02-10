c2893f516b4d6f211cb5c790ea24d1da90001a9a
salamalala <hello@ninaregli.ch>
2015-02-10 14:47:55 +0000

    getting also start lat & long working

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -3,16 +3,7 @@ class Journey < ActiveRecord::Base
   belongs_to :boat
   has_many :users, through: :bookings
 
-
-  geocoded_by :start_full_address, latitude: :start_port_latitude, longitude: :start_port_longitude
-
-  #specifies when to do the actual geo coding. 
-  after_validation :geocode
-
-  geocoded_by :end_full_address, latitude: :end_port_latitude, longitude: :end_port_longitude
-
-  #specifies when to do the actual geo coding. 
-  after_validation :geocode
+  before_save :geocode_addresses
 
   #takes all attributes and turns them into a string for google search
   def start_full_address
@@ -22,5 +13,40 @@ class Journey < ActiveRecord::Base
   def end_full_address
     [end_address, end_city, end_country].compact.join(', ')
   end
+
+  def start_full_address_changed?
+    [start_address_changed?, start_city_changed?, start_country_changed?].any?
+  end
+
+  def end_full_address_changed?
+    [end_address_changed?, end_city_changed?, end_country_changed?].any?
+  end
+
+
+#give a long and latitude to start full adress and end full adress. 
+# Send: When the method is identified by a string, the string is converted to a symbol. 
+  private
+  def geocode_addresses
+    %w(start end).each do |port|
+      if send("#{port}_address_changed?")
+        coords = Geocoder.coordinates(send("#{port}_full_address"))
+        send("#{port}_port_latitude=", coords[0])
+        send("#{port}_port_longitude=", coords[1])
+      end
+    end
+
+    # if start_address_changed?
+    #   coords = Geocoder.coordinates(self.start_full_address)
+    #   self.start_port_latitude = coords[0]
+    #   self.start_port_longitude = coords[1]
+    # end
+
+    # if end_address_changed?
+    #   coords = Geocoder.coordinates(self.end_full_address)
+    #   self.end_port_latitude = coords[0]
+    #   self.end_port_longitude = coords[1]
+    # end
+  end
+
 
 end
