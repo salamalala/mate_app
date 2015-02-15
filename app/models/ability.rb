dffb427dc358b87a13c8010ec7a2ef98e8d02b5d
salamalala <hello@ninaregli.ch>
2015-02-15 11:12:15 +0000

    adding color variables to variable.css.scss file and activate grid

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,8 +1,20 @@
-class Ability
-  include CanCan::Ability
+# class Ability
+#   include CanCan::Ability
 
-  def initialize(user)
-    user ||= User.new
- 
-  end
-end
+#   def initialize(user)
+#     user ||= User.new
+
+#     if user.role? :admin
+#       can :manage, :all
+
+#     elsif user.journeys_as_captain !nil   
+#       can [:update, :destroy], Journey do |journey|
+#         journey.user.id == user.id
+#         end
+#     else
+#       can :read, :all
+#     end
+
+#   end
+# end
+
